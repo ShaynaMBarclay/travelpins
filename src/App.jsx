06259a4795dab81301c18ad9d58502a3ea02a880
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-09-26 16:55:46 -0400

    css change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import ScrollToTop from "./Components/ScrollToTop";
 import CountryPage from "./Pages/CountryPage";
 import ChapterPage from "./Pages/ChapterPage";
 import Globe from "./Globe";
@@ -8,11 +9,12 @@ import "./Styles/App.css";
 function App() {
   return (
     <>
-        <Routes>
-          <Route path="/country/:countrySlug/:chapterSlug" element={<ChapterPage />} />
-          <Route path="/" element={<Globe />} />
-          <Route path="/country/:countrySlug" element={<CountryPage />} />
-        </Routes>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/country/:countrySlug/:chapterSlug" element={<ChapterPage />} />
+        <Route path="/" element={<Globe />} />
+        <Route path="/country/:countrySlug" element={<CountryPage />} />
+      </Routes>
     </>
   );
 }
